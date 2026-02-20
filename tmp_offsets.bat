54e4a4ad7e89ba78970738273576567318de5ec6
Fadli Heriyansyah <likertuban@gmail.com>
2026-02-20 08:19:31 +0700

    checkpoint

--- tmp_offsets.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 >nul
-cl /nologo /EHsc /std:c++17 /I".\WorldServer" /I".\WorldServer\Entities" /I".\WorldServer\ThirdParty\tinyxml" /I".\WorldServer\ThirdParty\lua-5.1.5\src" /I".\WorldServer\ThirdParty\cryptopp" tmp_offsets.cpp /link /OUT:tmp_offsets.exe
-if errorlevel 1 exit /b 1
-tmp_offsets.exe
--- tmp_test.cpp
+++
@@ -1,2 +0,0 @@
-class A { public: int f(int x){return x;} static A* Instance(){return nullptr;} };
-int main(){ A* a=A::Instance(); return A::f(a,1); }
